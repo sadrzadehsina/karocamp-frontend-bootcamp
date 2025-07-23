4d51a013675407ce97482117870dc67921a076f7
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2025-07-23 20:56:39 +0330

    Update

---
+++ 03_javascript/session-02-1404-05-01/script.js
@@ -0,0 +1,52 @@
+// String, Number, Boolean, null, undefined
+
+// [let|const] [variable_name] = [variable];
+
+// constant
+const a = "Hi"; // String
+const b = 10; // Number
+const c = true; // Boolean
+const d = false; // Boolean
+
+let e = "Hi"; // String
+let f = 10; // Number
+let g = true; // Boolean
+let h = false; // Boolean
+
+
+const num1 = 10;
+const num2 = 5;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+console.log(num1 % num2);
+
+const msg1 = "Hi";
+const space = " ";
+const msg2 = "John";
+
+console.log(msg1 + " " + msg2);
+
+let num3 = 10;
+num3 += 5;
+
+console.log(num3);
+
+let msg3 = "Hi";
+msg3 += " John";
+
+console.log(msg3);
+
+const var1 = 10;
+const var2 = 10;
+
+console.log(var1 === var2); // true
+console.log(var1 !== var2); // false
+
+const message1 = "Hi";
+const message2 = "HI";
+
+console.log(message1 === message2); // false
+console.log(message1 !== message2); // true
